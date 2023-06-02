c1e3713750ae49e959da6d7923d2a5d6e0208abd
Matt Zhang <set.stun@gmail.com>
2023-06-02 02:12:52 -0400

    Unit test fixes

--- elephant_news/tests/collection/test_congress.py
+++ elephant_news/tests/collection/test_congress.py
@@ -3,6 +3,6 @@ from elephant_news.collection import congress
 
 def test_scraper():
     scraper = congress.Scraper()
-    bills = scraper.scrape()
+    bills = scraper.scrape(from_today=False)
     assert len(bills) > 0
     assert type(bills[0]) == congress.Data
