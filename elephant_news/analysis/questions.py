15e00369a467d3a5a2d59537a3a2f8a446e9ae61
Matt Zhang <set.stun@gmail.com>
2023-06-01 21:43:54 -0400

    Added bias analysis questions

---
+++ elephant_news/analysis/questions.py
@@ -0,0 +1,10 @@
+1. Does the article present multiple perspectives or only one point of view?
+2. Does the article use emotive language to generate an emotional response from the reader?
+3. Does the article cite reliable sources that back up its claims, or is it based mainly on personal opinions or anecdotes?
+4. Does the article contain logical fallacies or factual errors that undermine the validity of the argument?
+5. Does the article omit any essential information that would present a fuller picture of the story?
+6. Does the article have a clear agenda or bias toward a particular perspective or group?
+7. Has the journalist provided enough information and sources to support the claims made in the article?
+8. Is the article objective in tone, or does it use unnecessarily provocative or sensationalist language?
+9. Does the writer have any pre-existing biases or affiliations that might influence their writing?
+10. Finally, would the article's content change if you were to change the headline?
