e3c3164176c39d994fe404bae077cf9aea9f4b3d
Matt Zhang <set.stun@gmail.com>
2023-06-02 23:07:07 -0400

    Added pytest flag for LLM tests

--- elephant_news/tests/analysis/test_claims.py
+++ elephant_news/tests/analysis/test_claims.py
@@ -1,3 +1,5 @@
+import pytest
+
 from elephant_news.analysis import claims
 from elephant_news.tests.analysis.article import article
 
@@ -8,6 +10,7 @@ def test_break_into_list():
     assert claims_list == ["claim 1", "claim 2", "claim 3"]
 
 
+@pytest.mark.llm
 def test_list_claims():
     article_text = article.title + "\n" + article.text + "\n" + "Related articles:\n" + "\n".join(article.related_articles)
     results = claims.list_claims(article_text)
