eabf7df5dc2d37a16019fd29393dc56a5591fd77
Matt Zhang <set.stun@gmail.com>
2023-06-06 18:23:59 -0400

    Removed unused analysis files

--- elephant_news/analysis/bias.py
+++
@@ -1,2 +0,0 @@
-Does the article have a clear agenda or bias toward a particular perspective or group?
-Does the writer have any pre-existing biases or affiliations that might influence their writing?
