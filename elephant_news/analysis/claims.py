c4fbbf4266b26917af25e89711f67bbbc00c1cba
Matt Zhang <set.stun@gmail.com>
2023-06-05 21:38:01 -0400

    Added prompt engineering to extract claims

--- elephant_news/analysis/claims.py
+++ elephant_news/analysis/claims.py
@@ -1,44 +1,27 @@
 from elephant_news.llm.llm import llm_api
 from elephant_news.llm.log import Log, Message
-
-
-def break_into_list(list_text: str) -> list[str]:
-    first_bracket = list_text.find("[")
-    last_bracket = list_text.rfind("]")
-    list_text = list_text[first_bracket + 1: last_bracket - 1]
-
-    split_claims = list_text.split("'")
-    split_claims = [s for s in split_claims if s != "" and s != ", "]
-    return split_claims
+from elephant_news.utils.text_parsing import break_into_list
 
 
 def list_claims(article: str) -> list[str]:
     log = Log("gpt-3.5-turbo")
-    message = Message(
-        "user",
-        "Summarize the claims in the following article.",
-    )
-    log.add_message(message)
-    message = Message(
-        "user",
-        article,
-    )
-    log.add_message(message)
-    claims = llm_api(log)
+    claims_query = f"""
+The article is delimited with triple backticks.
+Answer questions about the article by following these steps:
 
-    log.clear()
-    message = Message(
-        "user",
-        "Break these claims into a logical list of individual claims, like ['claim 1', 'claim 2', etc.]. Each claim should be as small as possible. Break complex claims into multiple smaller claims. Make sure any single quotes in claims are escaped with a backslash. Format claims into coherent sentences, and remove any extraneous markup like newlines, extra whitespace, odd capitalization, etc.",
-    )
-    log.add_message(message)
-    message = Message(
-        "user",
-        claims,
-    )
+- First, identify all claims in the article text.
+Each claim should be a single sentence, as short as possible.
+Break complex claims into multiple smaller claims.
+
+- Next, remove any empty claims.
+Format claims by escaping quotes, removing extra whitespace, and correcting capitalization.
+
+- Finally, format the claims as a Python list of strings.
+
+Article: '''{article}'''
+"""
+    message = Message("user", claims_query)
     log.add_message(message)
     claims_list_text = llm_api(log)
-    breakpoint()
-
     claims_list = break_into_list(claims_list_text)
     return claims_list
--- elephant_news/tests/analysis/test_claims.py
+++ elephant_news/tests/analysis/test_claims.py
@@ -4,12 +4,6 @@ from elephant_news.analysis import claims
 from elephant_news.tests.analysis.article import article
 
 
-def test_break_into_list():
-    claims_list_text = "['claim 1', 'claim 2', 'claim 3']"
-    claims_list = claims.break_into_list(claims_list_text)
-    assert claims_list == ["claim 1", "claim 2", "claim 3"]
-
-
 @pytest.mark.llm
 def test_list_claims():
     article_text = article.title + "\n" + article.text + "\n" + "Related articles:\n" + "\n".join(article.related_articles)
