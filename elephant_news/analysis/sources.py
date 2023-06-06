e9b41d1e71d342c746078be21ace4553e9e13ec0
Matt Zhang <set.stun@gmail.com>
2023-06-06 00:06:41 -0400

    Prompt engineering to extract cited sources

--- elephant_news/analysis/sources.py
+++ elephant_news/analysis/sources.py
@@ -1,23 +1,37 @@
+from dataclasses import dataclass
 import json
 
 from elephant_news.llm.llm import llm_api
 from elephant_news.llm.log import Log, Message
-from elephant_news.utils.text_parsing import break_into_list
 
 
-def list_sources(article: str) -> list[str]:
+@dataclass
+class Source:
+    id: int
+    description: str
+    source_type: str
+    is_primary_source: bool
+    is_available_online: bool
+
+
+def list_sources(article: str) -> list[Source]:
     log = Log("gpt-3.5-turbo")
     sources_query = f"""
 The article is delimited with triple backticks.
 List all sources cited to support the sources in the article.
-Format your response as a JSON list, with the following keys: SourceDescription (str), IsPrimarySource (bool), IsAvailableOnline (bool).
+Format your response as a JSON list, with the following keys:
+- id (int): an integer ID, with numbering starting from 0
+- description (str): a sentence describing what the source contains and where it's from
+- source_type (str): one of the following words: interview, speech, transcript, article, other
+- is_primary_source (bool)
+- is_available_online (bool): whether a copy of the original cited source can reasonably be expected to be found online
 
 Article: '''{article}'''
 """
-# TODO: include type of source
     message = Message("user", sources_query)
     log.add_message(message)
     sources_text = llm_api(log)
     sources = json.loads(sources_text)
+    sources = [Source(**source) for source in sources]
     breakpoint()
     return sources
