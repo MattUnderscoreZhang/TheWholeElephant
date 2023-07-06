185ec42c5bdde5b0c4ad46814d8f37650196c4e3
Matt Zhang <set.stun@gmail.com>
2023-07-06 01:21:03 -0400

    Added regex and test for CNN articles

--- browser/background/is_news_article.js
+++ browser/background/is_news_article.js
@@ -1,7 +1,8 @@
 // function to check if URL is a news article
 export function is_news_article_url(url) {
     const news_urls = [  // RegExp objects
-        /^https:\/\/www\.foxnews\.com\/(?!opinion\/)[^\/]+\/[^\/]+\/?.*$/
+        /^https:\/\/www\.foxnews\.com\/(?!opinion\/)[^\/]+\/[^\/]+\/?.*$/,
+        /^https:\/\/www\.cnn\.com\/\d{4}\/\d{2}\/\d{2}\/(?!opinions).*\/[^\/]+\/index\.html$/
     ]
     return news_urls.some((re) => re.test(url.href));
 }
--- browser/tests/test_is_news_article.js
+++ browser/tests/test_is_news_article.js
@@ -4,6 +4,7 @@ import {is_news_article_url} from "../background/is_news_article.js";
 export let tests = describe("is_news_article_url", function() {
     describe("Fox News", function() {
         it("Not Articles", function() {
+            assert.equal(is_news_article_url(new URL("https://www.foxnews.com")), false);
             assert.equal(is_news_article_url(new URL("https://www.foxnews.com/politics")), false);
             assert.equal(is_news_article_url(new URL("https://www.foxnews.com/world")), false);
             assert.equal(is_news_article_url(new URL("https://www.foxnews.com/opinion")), false);
@@ -19,6 +20,28 @@ export let tests = describe("is_news_article_url", function() {
 
         it("Opinion Articles", function() {
             assert.equal(is_news_article_url(new URL("https://www.foxnews.com/opinion/opinion_article")), false);
+        })
+    })
+
+    describe("CNN", function() {
+        it("Not Articles", function() {
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com")), false);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/world")), false);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/world/americas")), false);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/entertainment/tv-shows")), false);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/some_category/")), false);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/2023/07/05/category/index.html")), false);
+        })
+
+        it("News Articles", function() {
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/2023/07/05/entertainment/daniel-radcliffe-harry-potter-tv-series/index.html")), true);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/2023/07/05/us/philadelphia-shooting-wednesday/index.html")), true);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/2023/07/05/politics/taylor-taranto-detention-memo-obama-neighborhood-arrest/index.html")), true);
+        })
+
+        it("Opinion Articles", function() {
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/2023/07/04/opinions/supreme-court-online-harassment-university-of-chicago-alaimo/index.html")), false);
+            assert.equal(is_news_article_url(new URL("https://www.cnn.com/2023/06/30/opinions/student-loan-ruling-shows-trumps-supreme-court-plan-litman/index.html")), false);
         })
     })
 
